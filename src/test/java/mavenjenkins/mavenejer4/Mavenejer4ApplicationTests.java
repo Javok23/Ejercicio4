cc69a5820ac3158c009fb239ec1f9f471258280e
Javok23 <130308615+Javok23@users.noreply.github.com>
2023-04-11 15:14:36 +0200

    Add files via upload

---
+++ src/test/java/mavenjenkins/mavenejer4/Mavenejer4ApplicationTests.java
@@ -0,0 +1,13 @@
+package mavenjenkins.mavenejer4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mavenejer4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
